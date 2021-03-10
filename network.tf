2f56bf044720a1a77c6fc076668ebb585527232c
paul-batey-cko <paul.batey@checkout.com>
2021-03-10 17:30:54 +0000

    networking

--- network.tf
+++ network.tf
@@ -4,8 +4,6 @@ data "aws_availability_zones" "available" {
   state    = "available"
 }
 
-
-
 # Create public subnet in eu-west-2a
 resource "aws_subnet" "public_eu_west_2a" {
   provider          = aws.region-nginx
@@ -15,18 +13,6 @@ resource "aws_subnet" "public_eu_west_2a" {
 
   tags = {
     Name = "Public Subnet eu-west-2a"
-  }
-}
-
-# Create public subnet in eu-west-2b
-resource "aws_subnet" "public_eu_west_2b" {
-  provider          = aws.region-nginx
-  vpc_id            = var.vpc-id
-  cidr_block        = "172.31.51.0/28"
-  availability_zone = data.aws_availability_zones.available.names[1]
-
-  tags = {
-    Name = "Public Subnet eu-west-2b"
   }
 }
 
@@ -48,11 +34,5 @@ resource "aws_route_table" "web_vpc_public" {
 resource "aws_route_table_association" "web_vpc_eu_west_2a_public" {
   provider       = aws.region-nginx
   subnet_id      = aws_subnet.public_eu_west_2a.id
-  route_table_id = aws_route_table.web_vpc_public.id
-}
-
-resource "aws_route_table_association" "web_vpc_eu_west_2b_public" {
-  provider       = aws.region-nginx
-  subnet_id      = aws_subnet.public_eu_west_2b.id
   route_table_id = aws_route_table.web_vpc_public.id
 }
